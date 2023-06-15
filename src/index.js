994264a68e8bf5c9bff6212625f8fc2a73ba099d
le van hai <lvhai2k2@gmail.com>
2023-06-15 09:57:19 +0700

    template engines(handlebars)

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('node:path'); 
+const hbs = require('express-handlebars')
+
+
+const app = express()
+const port = 3000
+
+//HTTP logger
+app.use(morgan('combined'))
+
+// template engine
+app.set("view engine", 'hbs');
+app.engine('hbs', hbs.engine({
+  extname : '.hbs'
+}));
+
+//Static Folder
+app.set('views',path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
