ed9be30d34e418d66fa4cd6977f28e9d8df8e26d
le van hai <lvhai2k2@gmail.com>
2023-06-17 10:08:35 +0700

    routes&controller

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,18 @@
+class NewsController {
+    
+    // [GET] profile
+    index(req, res) {
+        res.render('profile');
+    }
+
+    // [GET] profile detail
+    show(req, res) {
+        res.send('profile detail')
+    }
+
+    haike(req, res) {
+        res.send('haikake')
+    }
+}
+
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
-const path = require('node:path'); 
+const path = require('node:path')
 const hbs = require('express-handlebars')
+const route = require('./routes')
 
 const app = express()
 const port = 3001
 
 //HTTP logger
 app.use(morgan('combined'))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 // template engine
 app.set("view engine", 'hbs');
@@ -21,13 +27,8 @@ app.set('views',path.join(__dirname, 'resources/views'))
 // static web 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/profile', (req, res) => {
-  res.render('profile');
-});
+// router init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
